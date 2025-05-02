cb0b6bfe83859347d63f6da6258d11bc4ecf0155
Borngreat Ikwutah <iagocech2056@gmail.com>
2025-05-02 12:50:18 +0100

    02: Customization for the Storefront..

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 
 export default function Home() {
   return (
-    <div className="p-2">
-      <p className="text-rose-500">Hello World</p>
+    <div className="flex flex-col gap-y-4 p-2">
+     <div>
+      <Button variant="elevated">
+        i am a button
+      </Button>
+     </div>
+
+     <div>
+      <Input placeholder="Type here..." />
+     </div>
+
+     <div>
+      <Progress value={50}/>
+     </div>
+
+     <div>
+      <Textarea placeholder="Type your message here..." />
+     </div>
+     
+     <div>
+      <Checkbox />
+     </div>
     </div>
   );
 }
